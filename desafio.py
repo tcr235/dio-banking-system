be0bfee85cc977c13c38fac34afb0df312defe67
Thiago Conte <tcr2@cin.ufpe.br>
2023-07-13 22:21:41 -0300

    BREAKING CHANGE: started modularization

--- desafio.py
+++ desafio.py
@@ -1,59 +1,87 @@
-menu = """
+def menu():
+    menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [nc] Nova conta
+    [lc] Listar contas
+    [nu] Novo usuário
+    [q] Sair
 
-"""
+    """
+    return input(menu)
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+def depositar():
+    pass
 
-while True:
-    opcao = input(menu)
+def sacar():
+    pass
 
-    if (opcao == "d"):
-        valor = float(input("Digite o valor a ser depositado: "))
+def exibir_extrato():
+    pass
 
-        if (valor > 0):
-            saldo += valor
-            extrato += f"Depósito: R${valor:.2f}\n"
+def criar_usuario():
+    pass
+
+def filtrar_usuario():
+    pass
+
+def criar_conta():
+    pass
+
+def listar_contas():
+    pass
+
+def main():
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+    AGENCIA = "0001"
+
+    while True:
+        opcao = input(menu)
+
+        if (opcao == "d"):
+            valor = float(input("Digite o valor a ser depositado: "))
+
+            if (valor > 0):
+                saldo += valor
+                extrato += f"Depósito: R${valor:.2f}\n"
+
+            else:
+                print("Operação não concluída: o valor informado é inválido.")
+
+        elif (opcao == "s"):
+            valor = float(input("Informe o valor do saque: "))
+            
+            if (valor > saldo):
+                print("Operação não concluída: não há saldo suficiente em conta.")
+
+            elif (valor > limite):
+                print("Operação não concluída: você excedeu o limite máximo de saque.")
+
+            elif (numero_saques > LIMITE_SAQUES):
+                print("Operação não concluída: você excedeu o limite máximo de saques diários.")
+
+            elif (valor > 0):
+                saldo -= valor
+                extrato += f"Saque: R${valor:.2f}\n"
+                numero_saques += 1
+
+            else:
+                print("Operação não concluída: o valor informado é inválido.")
+
+        elif (opcao == "e"):
+            print("\n=========== EXTRATO ===========")
+            print("Não foram realizadas movimentações." if not extrato else extrato)
+            print(f"\nSaldo: R${saldo:.2f}")
+            print("=================================")
+
+        elif (opcao == "q"):
+            break
 
         else:
-            print("Operação não concluída: o valor informado é inválido.")
-
-    elif (opcao == "s"):
-        valor = float(input("Informe o valor do saque: "))
-        
-        if (valor > saldo):
-            print("Operação não concluída: não há saldo suficiente em conta.")
-
-        elif (valor > limite):
-            print("Operação não concluída: você excedeu o limite máximo de saque.")
-
-        elif (numero_saques > LIMITE_SAQUES):
-            print("Operação não concluída: você excedeu o limite máximo de saques diários.")
-
-        elif (valor > 0):
-            saldo -= valor
-            extrato += f"Saque: R${valor:.2f}\n"
-            numero_saques += 1
-
-        else:
-            print("Operação não concluída: o valor informado é inválido.")
-
-    elif (opcao == "e"):
-        print("\n=========== EXTRATO ===========")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R${saldo:.2f}")
-        print("=================================")
-
-    elif (opcao == "q"):
-        break
-
-    else:
-        print("Operação inválida, por favor digite novamente a operação desejada.")
+            print("Operação inválida, por favor digite novamente a operação desejada.")
